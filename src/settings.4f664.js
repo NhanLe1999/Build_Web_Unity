b569ce2c20cdac8c05c5c1139c97e724116aaa30
NhanLe1999 <55460318+NhanLe1999@users.noreply.github.com>
2024-06-28 09:39:23 +0700

    update flappy-bird

---
+++ src/settings.4f664.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["bird","ground","pipe"],collisionMatrix:[[false,true,true],[true,false],[true,false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",main:"de8c6"}};
