d0ae13571feb24668dc6a4a669d8f57be83573e4
NhanLe1999 <55460318+NhanLe1999@users.noreply.github.com>
2024-06-28 09:51:26 +0700

    update

--- src/settings.4f664.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["bird","ground","pipe"],collisionMatrix:[[false,true,true],[true,false],[true,false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",main:"de8c6"}};
